a53ff262034e503e9243c33f4c8d9231f1cf5a0a
berrones <adamberrones@protonmail.com>
2024-10-02 14:27:01 -0400

    add dat directory; add atlas_data.csv; add Mistry\'s R code; create adam.Rmd

--- R/global.R
+++ R/global.R
@@ -9,9 +9,3 @@ library(bslib)
 library(dplyr)
 library(readr)
 
-source('/home/adam/Sandbox/adam-utils/sysOpsLibsFuncs.R')
-# wrangling ---------------------------------------------------------------
-
-geneExpressionData <- readRDS('/mnt/KDPH/PQI/PQI Data Analytics/BrainTumorAtlas-main/Gene Expression Data/ExpData.rds')
-
-atlasData <- read_csv('/mnt/KDPH/PQI/PQI Data Analytics/BrainTumorAtlas-main/Atlas_Data.csv')
