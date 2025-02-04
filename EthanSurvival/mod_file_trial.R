50f9069194f65d5bd66a4b563adf5ae63357ece8
Ethan Heck <ethanheck@Ethans-MacBook-Pro.local>
2025-02-03 19:47:25 -0500

    changed method of plotting to compare methods, exploring strata

--- EthanSurvival/mod_file_trial.R
+++ EthanSurvival/mod_file_trial.R
@@ -4,6 +4,7 @@
 library(survival)
 library(survminer) 
 library(ggplot2) 
+library(dplyr)
 ##################################
 
 
@@ -31,12 +32,71 @@ library(ggplot2)
 # annotate("text", x=45, y=1, label= "GCT (n=6)",hjust = 0)
 
 ####Getting complete cases for survival######
-atlasDataClean_2 <- subset(atlasDataClean,atlasDataClean$survivalMonths != "NA")
+atlasDataClean <- subset(atlasDataClean,atlasDataClean$survivalMonths != "NA")
 
 #surv model
 fit <- survfit(Surv(time = as.numeric(survivalMonths), event = mortality) ~ grade , 
-               data = atlasDataClean_2)
+               data = atlasDataClean)
 
 
 
 ggsurvplot(fit, risk.table = F, legend='right', xlab="Time (Months)")
+
+
+####creating my own model, looking at survival stratified by grade w in those w diagnosis
+###CNS #####
+atlasDataClean_sub <- subset(atlasDataClean, atlasDataClean$diagnosis == "CNS")
+
+fit <- survfit(Surv(time = as.numeric(survivalMonths), event = mortality) ~ grade , 
+               data = atlasDataClean)
+ggsurvplot(fit, risk.table = F, legend='right', xlab="Time (Months)", title = "Survival (Months) 
+           Among CNS Diagnosis Stratified by Grade")
+
+
+
+
+
+
+
+
+
+############TRYING ALTERNATIVE METHOD FOR PLOTTING KP CURVES##################
+####converting surv obj to df allows for more graph control/customization easily via
+###using ggplot2 directly#################
+
+survfit(Surv(time = as.numeric(survivalMonths), event = mortality) ~ grade , 
+        data = atlasDataClean)
+# Example survival model (stratified by 4 groups)
+fit <- survfit(Surv(time = as.numeric(survivalMonths), event = mortality) ~ grade , 
+               data = atlasDataClean) 
+
+# Convert survfit object to tidy data
+surv_df <- as.data.frame(summary(fit)[c("time", "surv", "strata", "lower", "upper")])
+
+# Rename columns for convenience
+colnames(surv_df) <- c("time", "survival", "group", "lower", "upper")
+
+
+
+# Create the Kaplan-Meier plot using ggplot
+ggplot(surv_df, aes(x = time, y = survival, color = group, fill = group)) +
+  geom_step(size = 1.2) +  # Kaplan-Meier survival curve
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2, color = NA) +  # Confidence intervals
+  labs(
+    title = "Survival Probability (Months) ",
+    x = "Time (Months)",
+    y = "Survival Probability",
+    color = "Group",
+    fill = "Group"
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+    legend.position = "right"
+  )
+
+
+
+
+
+###
