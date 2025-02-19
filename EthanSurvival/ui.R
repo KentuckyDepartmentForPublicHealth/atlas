4ca3c48075fc79c525278d2790bb8333d26a1cf3
Ethan Heck <ethanheck@outlook.com>
2025-02-19 13:05:09 -0500

    changed Hr table output to gt table for visual clarity

--- EthanSurvival/ui.R
+++ EthanSurvival/ui.R
@@ -9,10 +9,7 @@
 
 library(shiny)
 
-# Define UI using navbarPage for two tabs
-ui <- navbarPage(
-  title = "Atlas: Survival Analysis",
-  # CSS to custom color top bar ---- had to generate template w chat gpt
+ui <- tagList(
   tags$head(tags$style(HTML("
     .navbar {
       background-color: #ADD8E6 !important; /* Light blue background */
@@ -29,38 +26,35 @@ ui <- navbarPage(
       color: #ff5733 !important; /* Orange hover effect for links */
     }
   "))),
-  # Tab 1
-  tabPanel(
-    "Kaplan Meier Curves",
-    fluidPage(
-      # Title
-      titlePanel("Kaplan Meier Curves with Simulated Dataset"),
-      
-      # Sidebar with input
-      sidebarLayout(
-        sidebarPanel(
-          selectInput(
-            inputId = 'diagnosis',
-            label = 'Select Diagnosis:', # What user sees
-            choices = unique_diagnosis, # Choices are each unique value from the bmi variable
-            selected = unique_diagnosis[1] # Default choice set to first index
+  navbarPage(
+    title = "Atlas: Survival Analysis",
+    tabPanel(
+      "Kaplan Meier Curves",
+      fluidPage(
+        titlePanel("Kaplan Meier Curves with Simulated Dataset"),
+        sidebarLayout(
+          sidebarPanel(
+            selectInput(
+              inputId = 'diagnosis',
+              label = 'Select Diagnosis:',
+              choices = unique_diagnosis,
+              selected = unique_diagnosis[1]
+            ),
+            selectInput(
+              inputId = 'Strata',
+              label = 'Select strata:',
+              choices = allowed_vars,
+              selected = allowed_vars[1]
+            ),
+            checkboxInput("show_hr", "Display Hazard Ratios", value = FALSE)
           ),
-          selectInput(
-            inputId = 'Strata',
-            label = 'Select strata:', # What user sees
-            choices = allowed_vars, # Choices are each unique value from the bmi variable
-            selected = allowed_vars[1] # Default choice set to first index
-          ),
-          checkboxInput("show_hr", "Display Hazard Ratios", value = FALSE)
-        ),
-        mainPanel(
-          plotOutput("kmplt"), # Output for Kaplan-Meier plot
-          verbatimTextOutput("hazard_ratios") # Output for hazard ratios
+          mainPanel(
+            plotOutput("kmplt"),
+            gt_output("hazard_table")
+          )
         )
       )
     )
   )
 )
-
-
 
