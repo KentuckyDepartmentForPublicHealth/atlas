b30134b386e89e071fb34f7ffd3bda021a40372a
Ethan Heck <ethanheck@outlook.com>
2025-02-19 13:27:59 -0500

    added risk table toggle functionality

--- EthanSurvival/ui.R
+++ EthanSurvival/ui.R
@@ -46,7 +46,8 @@ ui <- tagList(
               choices = allowed_vars,
               selected = allowed_vars[1]
             ),
-            checkboxInput("show_hr", "Display Hazard Ratios", value = FALSE)
+            checkboxInput("show_hr", "Display Hazard Ratios", value = FALSE),
+            checkboxInput("show_risk_table", "Show Risk Table", value = FALSE)
           ),
           mainPanel(
             plotOutput("kmplt"),
