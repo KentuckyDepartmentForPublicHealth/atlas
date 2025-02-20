aa586fca806758271a4842432eb436e77f8c921d
Ethan Heck <ethanheck@outlook.com>
2025-02-20 11:05:34 -0500

    fixed NA censoring issues, toggle for censoring marks

--- EthanSurvival/global.R
+++ EthanSurvival/global.R
@@ -9,11 +9,13 @@ library(ggsurvfit)
 
 #data
 load("~/atlas/dat/atlasDataClean.RData")
-atlasDataClean <- subset(atlasDataClean,atlasDataClean$survivalMonths != "NA" & atlasDataClean$mortality != "NA")
+atlasDataClean <- subset(atlasDataClean,atlasDataClean$survivalMonths != "NA")
+atlasDataClean$mortality <- ifelse(is.na(atlasDataClean$mortality), 0, atlasDataClean$mortality)
+
 atlasDataClean$survivalMonths <- as.numeric(atlasDataClean$survivalMonths)
 #strata
 allowed_vars <- c("ageGroup", "tumorType", "grade", "sex", "locationOriginal","compartment","histologyOriginal")
 
 
-unique_diagnosis <- unique(atlasDataClean$diagnosis)
+unique_diagnosis <- unique(atlasDataClean$diagnosisFinal)
 
