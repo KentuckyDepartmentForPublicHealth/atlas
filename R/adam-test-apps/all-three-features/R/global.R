0756fba8a148e072948862db1c3eb41c06158a59
berrones <adamberrones@protonmail.com>
2025-04-08 12:19:50 -0400

    link kdph and kcrtf in footer

--- R/adam-test-apps/all-three-features/R/global.R
+++ R/adam-test-apps/all-three-features/R/global.R
@@ -5,6 +5,6 @@
 # time --------------------------------------------------------------------
 
 
-# currentDate <- format(Sys.time(), '%a, %b %d, %Y at %I:%M %p EDT')
+# currentDate <- format(Sys.time(), '%a, %b %d, %Y at %I:%M %p ET')
 # saveRDS(currentDate, file = 'dat/currentDate.rds')
 currentDate <- readRDS(file = "dat/currentDate.rds")
