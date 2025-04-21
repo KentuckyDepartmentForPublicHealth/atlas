0a65a38998d4e2770ab60d908cb28e3903e7493d
berrones <adamberrones@protonmail.com>
2025-04-21 16:13:00 -0400

    current date path in global.R updated

--- R/adam-test-apps/all-three-features/R/global.R
+++ R/adam-test-apps/all-three-features/R/global.R
@@ -6,5 +6,6 @@
 
 
 # currentDate <- format(Sys.time(), '%a, %b %d, %Y at %I:%M %p ET')
-# saveRDS(currentDate, file = 'dat/currentDate.rds')
+# saveRDS(currentDate, file = paste0(getwd(), '/R/adam-test-apps/all-three-features/dat/currentDate.rds'))
 currentDate <- readRDS(file = "dat/currentDate.rds")
+getwd()
