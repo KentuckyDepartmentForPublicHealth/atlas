3f43747f81e16bcaca3cda857e716704d2cbbcb2
berrones <adamberrones@protonmail.com>
2025-05-21 09:39:16 -0400

    deploy: update [2025-05-21]

--- deploy.R
+++ deploy.R
@@ -3,6 +3,6 @@ APP_TIMESTAMP <- format(Sys.time(), "%a, %b %d, %Y at %I:%M %p ET")
 saveRDS(APP_TIMESTAMP, file = "dat/APP_TIMESTAMP.rds")
 
 # Deploy the application
-rsconnect::deployApp(appName = "atlas-dev99", logLevel = "verbose")
+rsconnect::deployApp(appName = "atlas", logLevel = "verbose")
 
 cat("Deployment completed with timestamp:", APP_TIMESTAMP, "\n")
